88d9fa59fcef4e249c14f6fc5ce06ef5097d9753
shouma237 <jonatha.mariano15@gmail.com>
2023-11-25 14:21:32 +0800

    final commit

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,40 +1,25 @@
-
 import throttle from 'lodash.throttle';
 
-const form = document.querySelector('.feedback-form');
-const inputs = form.querySelectorAll('input, textarea');
-let saveStateThrottled = null;
-
-function saveState() {
- const formData = {
-    email: inputs[0].value,
-    message: inputs[1].value,
- };
- localStorage.setItem('feedback-form-state', JSON.stringify(formData));
- console.log(formData);
+const formSubmit = document.querySelector('.feedback-form');
+const value = localStorage.getItem('feedback-form-state');
+if (value !== null) {
+  const parseValue = JSON.parse(value);
+  const { email, message } = formSubmit;
+  email.value = parseValue.email;
+  message.value = parseValue.message;
 }
 
-function throttledSaveState() {
- if (!saveStateThrottled) {
-    saveStateThrottled = setTimeout(() => {
-      saveState();
-      saveStateThrottled = null;
-    }, 500);
- }
-}
+const formHandler = event => {
+  event.preventDefault();
+  console.log(JSON.parse(localStorage.getItem('feedback-form-state')));
+  localStorage.removeItem('feedback-form-state');
+  event.currentTarget.reset();
+};
+const inputHandler = event => {
+  const { email, message } = event.currentTarget;
+  const formItems = { email: email.value, message: message.value };
+  localStorage.setItem('feedback-form-state', JSON.stringify(formItems));
+};
 
-form.addEventListener('input', throttledSaveState);
-
-form.addEventListener('submit', (event) => {
- event.preventDefault();
- saveState();
- localStorage.removeItem('feedback-form-state');
- inputs[0].value = '';
- inputs[1].value = '';
-});
-
-const storedData = localStorage.getItem('feedback-form-state');
-if (storedData) {
- const formData = JSON.parse(storedData);
- inputs[0].value = formData.email;
- inputs[1].value = formData.message;
+formSubmit.addEventListener('input', inputHandler);
+formSubmit.addEventListener('submit', formHandler);
